9564d553c865d9fe6b980f4138fb44e395b9727d
Shivam Nishad <shivam@Shivams-MacBook-Air.local>
2025-03-24 13:42:56 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # DocEase
-# DocEase
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Booking a doctor’s appointment online is a quick and convenient way to access healthcare services without the hassle of long wait times or phone calls. Many medical clinics, hospitals, and telehealth platforms offer online appointment scheduling, allowing patients to choose their preferred doctor, date, and time with just a few clicks.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+91 888 77 56715</li>
+            <li>shivam.nishad.psit@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025 @ docease.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
